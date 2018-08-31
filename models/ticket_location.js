0a4c9396e2eb7b9fb649a64ef097ca349065fccb
vsaraogi <mailvsaraogi@gmail.com>
2018-08-30 21:22:52 -0400

    Made files eslint compliant. Removed index.test.js

--- models/ticket_location.js
+++ models/ticket_location.js
@@ -1,8 +1,7 @@
-module.exports = function(sequelize, DataTypes) {
-    var TicketLocation = sequelize.define("TicketLocation", {
-      category: DataTypes.STRING,
-      location: DataTypes.GEOMETRY('POINT')
-    });
-    return TicketLocation;
-  };
-  
+module.exports = (sequelize, DataTypes) => {
+  const TicketLocation = sequelize.define('TicketLocation', {
+    category: DataTypes.STRING,
+    location: DataTypes.GEOMETRY('POINT'),
+  });
+  return TicketLocation;
+};
