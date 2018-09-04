a820fc1e63af143eb0561805f9fce0b96677ba83
vsaraogi <mailvsaraogi@gmail.com>
2018-09-04 00:00:26 -0400

    Added comment for regex string

--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -3,6 +3,10 @@ const faker = require('faker');
 
 module.exports = (app) => {
   app.post('/api/user-registration', (req, resp) => {
+    // Regex to get the first name and last name
+    // Any input string, delimited by space, which 
+    // has more than 1 word, the last word will be
+    // assigned to last name
     const re = /(([a-zA-Z\s]+)\s([a-zA-Z]+))|([a-zA-Z]+)/;
     const arr = req.body.name.match(re);
     // VIK_TODO: Assert that index 2 or 4 is defined
