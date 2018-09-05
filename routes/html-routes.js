066159a32f953281f67dd13d3c07b9efc0796d35
palshah <palakshah28@gmail.com>
2018-09-04 21:32:39 -0400

    Registartion Route Changes - Palak

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,4 +4,7 @@ module.exports = (app) => {
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/mapindex.html'));
   });
+  app.get("/registration", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/registration.html"));
+  });
 };
