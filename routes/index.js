8473cf08056ec3b43ba3ed526e6980eff13be0af
Travis Collins <trc229@gmail.com>
2018-09-05 21:40:58 -0400

    ESLint fixes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-module.exports = app => {
+module.exports = (app) => {
   app.get('/', (req, res) => {
     res.render('index');
   });
-}
+};
--- routes/tickets.js
+++ routes/tickets.js
@@ -1,7 +1,7 @@
 const ticketController = require('../controllers/ticket-controller');
 
-module.exports = app => {
+module.exports = (app) => {
   app.get('/api/tickets', (req, res) => {
     res.send('NOT IMPLEMENTED: Site Homepage');
   });
-}
+};
