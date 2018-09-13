e7a9f6a3d21da121100f16d3b8ca83f06ef6a1da
Travis Collins <trc229@gmail.com>
2018-09-12 23:58:17 -0400

    Remove login/registration pages after consolidation

--- public/assets/js/registration.js
+++ public/assets/js/registration.js
@@ -7,32 +7,6 @@ $(document).ready(() => {
   const password = $('#password');
   const username = $('#username');
 
-  getCookie = (name) => {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) {
-      return parts.pop().split(';').shift();
-    }
-    return undefined;
-  };
-
-  deleteCookie = (name) => {
-    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
-  };
-
-  const uuid = getCookie('kaizen72441_uuid');
-
-  if (uuid) {
-    const ticketLink = '<h2><a href="/createTicket.html">Improve It!</a><h2>';
-    const logoutLink = '<p><a href="#">Logout</a>';
-    $('#ticket-link').append(ticketLink);
-    $('#login-links').append(logoutLink);
-  } else {
-    const loginLinks = '<p><a href="/login">Login</a> </p>';
-    $('#login-links').append(loginLinks);
-  }
-
-
   // Adding an event listener for when the form is submitted
   $('#registration').on('submit', (event) => {
     event.preventDefault();
--- routes/registration.js
+++
@@ -1,5 +0,0 @@
-module.exports = (app) => {
-  app.get('/registration', (req, res) => {
-    res.render('registration', { title: 'Registration', js: ['registration.js'] });
-  });
-};
