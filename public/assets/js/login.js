709ce328e0770ff2d4063305fc874317be53d27a
Travis Collins <trc229@gmail.com>
2018-09-12 23:30:19 -0400

    Update homepage style

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,6 +1,31 @@
 /* eslint-env jquery */
 
 $(() => {
+  getCookie = (name) => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) {
+      return parts.pop().split(';').shift();
+    }
+    return undefined;
+  };
+
+  deleteCookie = (name) => {
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+  };
+
+  const uuid = getCookie('kaizen72441_uuid');
+
+  if (uuid) {
+    const ticketLink = '<h2><a href="/createTicket.html">Improve It!</a><h2>';
+    const logoutLink = '<p><a href="#">Logout</a>';
+    $('#ticket-link').append(ticketLink);
+    $('#login-links').append(logoutLink);
+  } else {
+    const loginLinks = '<p><a href="/registration">Register</a> </p>';
+    $('#login-links').append(loginLinks);
+  }
+
   $('#login').on('submit', (evt) => {
     evt.preventDefault();
 
